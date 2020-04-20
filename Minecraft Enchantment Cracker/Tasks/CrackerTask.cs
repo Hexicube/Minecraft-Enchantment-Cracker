75c17d4dcaa65eadeb8c2775184efa88eab4bd8a
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-04-20 14:50:23 -0400

    Remove RNG class, save 20 seconds at the cost of a monolith file

--- Minecraft Enchantment Cracker/Tasks/CrackerTask.cs
+++ Minecraft Enchantment Cracker/Tasks/CrackerTask.cs
@@ -1,65 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Minecraft_Enchantment_Cracker.Tasks {
-    public class CrackerTask : IProgressiveTask {
-        private static int GetGenericEnchantability(ref long random, int shelves) {
-            int first = random.NextInt(8);
-            int second = random.NextInt(shelves+1);
-            return first + 1 + (shelves >> 1) + second;
-        }
-
-        private static int GetLevelsSlot1(ref long random, int shelves) {
-            int enchantability = GetGenericEnchantability(ref random, shelves) / 3;
-            return enchantability < 1 ? 1 : enchantability;
-        }
-
-        private static int GetLevelsSlot2(ref long random, int shelves) {
-            return GetGenericEnchantability(ref random, shelves) * 2 / 3 + 1;
-        }
-
-        private static int GetLevelsSlot3(ref long random, int shelves) {
-            int enchantability = GetGenericEnchantability(ref random, shelves);
-            int twiceShelves = shelves * 3;
-            return enchantability < twiceShelves ? twiceShelves : enchantability;
-        }
-
+namespace Minecraft_Enchantment_Cracker.Tasks
+{
+    public class CrackerTask : IProgressiveTask
+    {
         private int progressAmt, progressMax;
-        public float Progress { get {
-            if (progressMax == -1) return (float)((long)progressAmt - (long)int.MinValue) / (float)((long)int.MaxValue - (long)int.MinValue);
-            return (float)progressAmt / (float)progressMax;
-        } }
-        public bool Success => LastSeedsFound != 0;
-        private int LastSeedsFound = -1;
-        private int TheSeed;
-        private long LastSearchTime = 0;
 
-        private static string LongToQty(long v) {
-            if(v > 1000000000) return$"{v / 1000000000f:0.00}B";
-            if(v > 100000000) return$"{v / 1000000f:000}M";
-            if(v > 10000000) return$"{v / 1000000f:00.0}M";
-            if(v > 1000000) return$"{v / 1000000f:0.00}M";
-            if(v > 100000) return$"{v / 1000f:000}K";
-            if(v > 10000) return$"{v / 1000f:00.0}K";
-            if(v > 1000) return$"{v / 1000f:0.00}K";
+        public float Progress
+        {
+            get
+            {
+                if(progressMax == -1)
+                    return(float)((long)progressAmt - (long)int.MinValue) /
+                          (float)((long)int.MaxValue - (long)int.MinValue);
+
+                return(float)progressAmt / (float)progressMax;
+            }
+        }
+
+        public  bool Success => LastSeedsFound != 0;
+        private int  LastSeedsFound = -1;
+        private int  TheSeed;
+        private long LastSearchTime;
+
+        private static string LongToQty(long v)
+        {
+            if(v > 1000000000)
+                return$"{v / 1000000000f:0.00}B";
+
+            if(v > 100000000)
+                return$"{v / 1000000f:000}M";
+
+            if(v > 10000000)
+                return$"{v / 1000000f:00.0}M";
+
+            if(v > 1000000)
+                return$"{v / 1000000f:0.00}M";
+
+            if(v > 100000)
+                return$"{v / 1000f:000}K";
+
+            if(v > 10000)
+                return$"{v / 1000f:00.0}K";
+
+            if(v > 1000)
+                return$"{v / 1000f:0.00}K";
 
             return$"{v}";
         }
 
-        public string ProgressText { get {
-            if (LastSeedsFound == -1) {
-                if (progressMax == -1) return $"{LongToQty((long)progressAmt - (long)int.MinValue)} / {LongToQty((long)int.MaxValue - (long)int.MinValue)}";
-                return $"{LongToQty(progressAmt)} / {LongToQty(progressMax)}";
+        public string ProgressText
+        {
+            get
+            {
+                switch(LastSeedsFound) {
+                    case -1:
+                        return progressMax == -1 ? $"{LongToQty((long)progressAmt - (long)int.MinValue)} / {LongToQty((long)int.MaxValue - (long)int.MinValue)}" : $"{LongToQty(progressAmt)} / {LongToQty(progressMax)}";
+                    case 0:
+                        return"No seeds found";
+                    case 1:
+                        return$"Seed: {TheSeed:X8}";
+                    default:
+                        return$"Seeds found: {LongToQty(LastSeedsFound)}";
+                }
             }
-            if (LastSeedsFound == 0) return "No seeds found";
-            if (LastSeedsFound == 1) return $"Seed: {TheSeed.ToString("X8")}";
-            return $"Seeds found: {LongToQty(LastSeedsFound)}";
-        } }
-        public string ProgressText2 { get {
-            if (LastSeedsFound == -1) return $"{((int)(Progress*100)).ToString("00")}%";
-            return $"Took {(LastSearchTime/1000f).ToString("0.0")}s";
-        } }
-        public List<int> GetSeeds(int shelves, int slot1, int slot2, int slot3, List<int> priorSeeds) {
+        }
+
+        public string ProgressText2 => LastSeedsFound == -1 ? $"{(int)(Progress * 100):00}%" : $"Took {LastSearchTime / 1000f:0.0}s";
+
+        public List<int> GetSeeds(int shelves, int slot1, int slot2, int slot3, List<int> priorSeeds)
+        {
             LastSeedsFound = -1;
             long start = Environment.TickCount;
 
@@ -81,73 +92,176 @@ namespace Minecraft_Enchantment_Cracker.Tasks {
 
             // temp values
             int ench1, ench2, ench3;
-            
             long seed;
 
-            if (priorSeeds == null) {
+            if(priorSeeds == null)
+            {
                 // reasonable guess (based on 15:7/17/30 being ~80M)
                 result = new List<int>(100000000);
-                
+
                 progressAmt = int.MinValue;
                 progressMax = -1; // progress has explicit check for this
-                if (shelvesPlusOne == 16) {
-                    do {
-                        seed = progressAmt ^ JavaRandom.M & JavaRandom.Mask;
-                    
-                        ench1 = seed.NextTwoInt16();
-                        if (ench1 < slot1low || ench1 > slot1high) { progressAmt++; continue; }
-
-                        ench2 = (seed.NextTwoInt16() + halfShelves) * 2 / 3;
-                        if (ench2 != secondSubOne) { progressAmt++; continue; }
-
-                        result.Add(progressAmt++);
-                    }
-                    while (progressAmt != int.MinValue);
-                    progressAmt = int.MaxValue;
-                }
-                else if ((shelvesPlusOne & -shelvesPlusOne) == shelvesPlusOne) {
+                if(shelvesPlusOne == 16)
+                {
                     do
                     {
                         seed = progressAmt ^ JavaRandom.M & JavaRandom.Mask;
 
-                        ench1 = seed.NextTwoIntP2(shelvesPlusOne);
-                        if(ench1 < threeSubHalf) { if (slot1 != 1) { progressAmt++; continue; } }
-                        if(ench1 < slot1low || ench1 > slot1high) { progressAmt++; continue; }
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        var temprng = (int)((ulong)seed >> 45) & 7;
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        ench1 = temprng + ((int)((ulong)seed >> 44) & 15);
+                        if(ench1 < slot1low || ench1 > slot1high)
+                        {
+                            progressAmt++;
+                            continue;
+                        }
 
-                        ench2 = (seed.NextTwoIntP2(shelvesPlusOne) + halfShelves) * 2 / 3;
-                        if(ench2 != secondSubOne) { progressAmt++; continue; }
-
-                        if(!early3) {
-                            ench3 = (seed.NextTwoIntP2(shelvesPlusOne) + halfShelves);
-                            if(Math.Max(ench3, twoShelves) != slot3) { progressAmt++; continue; }
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        temprng = (int)((ulong)seed >> 45) & 7;
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        ench2 = (temprng + ((int)((ulong)seed >> 44) & 15) + halfShelves) * 2 / 3;
+                        if(ench2 != secondSubOne)
+                        {
+                            progressAmt++;
+                            continue;
                         }
 
                         result.Add(progressAmt++);
-                    }
-                    while(progressAmt != int.MinValue);
+                    } while(progressAmt != int.MinValue);
+
                     progressAmt = int.MaxValue;
                 }
-                else {
-                    do {
+                else if((shelvesPlusOne & -shelvesPlusOne) == shelvesPlusOne)
+                {
+                    do
+                    {
                         seed = progressAmt ^ JavaRandom.M & JavaRandom.Mask;
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        var temprng = (int)((ulong)seed >> 45) & 7;
 
-                        ench1 = seed.NextTwoIntNotP2(shelvesPlusOne);
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        ench1 = temprng + (int)((shelvesPlusOne * (long)((ulong)seed >> 17)) >> 31);
                         if(ench1 < threeSubHalf)
-                            if(slot1 != 1) { progressAmt++; continue; }
+                            if(slot1 != 1)
+                            {
+                                progressAmt++;
+                                continue;
+                            }
 
-                        if(ench1 < slot1low || ench1 > slot1high) { progressAmt++; continue; }
+                        if(ench1 < slot1low || ench1 > slot1high)
+                        {
+                            progressAmt++;
+                            continue;
+                        }
 
-                        ench2 = (seed.NextTwoIntNotP2(shelvesPlusOne) + halfShelves) * 2 / 3;
-                        if(ench2 != secondSubOne) { progressAmt++; continue; }
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        temprng = (int)((ulong)seed >> 45) & 7;
 
-                        if(!early3) {
-                            ench3 = seed.NextTwoIntNotP2(shelvesPlusOne) + halfShelves;
-                            if(Math.Max(ench3, twoShelves) != slot3) { progressAmt++; continue; }
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        ench2 = (temprng + (int)((shelvesPlusOne * (long)((ulong)seed >> 17)) >> 31) + halfShelves) *
+                                2 /
+                                3;
+
+                        if(ench2 != secondSubOne)
+                        {
+                            progressAmt++;
+                            continue;
+                        }
+
+                        if(!early3)
+                        {
+                            seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                            temprng = (int)((ulong)seed >> 45) & 7;
+
+                            seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                            ench3 = temprng + (int)((shelvesPlusOne * (long)((ulong)seed >> 17)) >> 31) + halfShelves;
+                            if(Math.Max(ench3, twoShelves) != slot3)
+                            {
+                                progressAmt++;
+                                continue;
+                            }
                         }
 
                         result.Add(progressAmt++);
-                    }
-                    while(progressAmt != int.MinValue);
+                    } while(progressAmt != int.MinValue);
+
+                    progressAmt = int.MaxValue;
+                }
+                else
+                {
+                    do
+                    {
+                        seed = progressAmt ^ JavaRandom.M & JavaRandom.Mask;
+
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        var temprng = (int)((ulong)seed >> 45) & 7;
+
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        int tempval;
+                        int r;
+                        do
+                        {
+                            seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                            r = (int)((ulong)seed >> 17);
+                            tempval = r % shelvesPlusOne;
+                        } while(r - tempval + shelvesPlusOne < -1);
+
+                        ench1 = temprng + tempval;
+                        if(ench1 < threeSubHalf)
+                            if(slot1 != 1)
+                            {
+                                progressAmt++;
+                                continue;
+                            }
+
+                        if(ench1 < slot1low || ench1 > slot1high)
+                        {
+                            progressAmt++;
+                            continue;
+                        }
+
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        temprng = (int)((ulong)seed >> 45) & 7;
+
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        do
+                        {
+                            seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                            r = (int)((ulong)seed >> 17);
+                            tempval = r % shelvesPlusOne;
+                        } while(r - tempval + shelvesPlusOne < -1);
+
+                        ench2 = (temprng + tempval + halfShelves) * 2 / 3;
+                        if(ench2 != secondSubOne)
+                        {
+                            progressAmt++;
+                            continue;
+                        }
+
+                        if(!early3)
+                        {
+                            seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                            temprng = (int)((ulong)seed >> 45) & 7;
+
+                            seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                            do
+                            {
+                                seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                                r = (int)((ulong)seed >> 17);
+                                tempval = r % shelvesPlusOne;
+                            } while(r - tempval + shelvesPlusOne < -1);
+
+                            ench3 = (temprng + tempval + halfShelves);
+                            if(Math.Max(ench3, twoShelves) != slot3)
+                            {
+                                progressAmt++;
+                                continue;
+                            }
+                        }
+
+                        result.Add(progressAmt++);
+                    } while(progressAmt != int.MinValue);
 
                     progressAmt = int.MaxValue;
                 }
@@ -167,7 +281,21 @@ namespace Minecraft_Enchantment_Cracker.Tasks {
                     // less efficient, but runs on far fewer seeds
                     seed = s ^ JavaRandom.M & JavaRandom.Mask;
 
-                    ench1 = seed.NextTwoInt(shelvesPlusOne);
+                    seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                    var temprng = (int)((ulong)seed >> 45) & 7;
+
+                    seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                    var temprng2 = (int)((ulong)seed >> 17);
+                    if((shelvesPlusOne & shelvesPlusOne - 1) == 0)
+                        temprng2 = (int)((shelvesPlusOne * (long)temprng2) >> 31);
+                    else
+                        while(temprng2 - (temprng2 %= shelvesPlusOne) + shelvesPlusOne - 1 < 0)
+                        {
+                            seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                            temprng2 = (int)((ulong)seed >> 17);
+                        }
+
+                    ench1 = temprng + temprng2;
                     if(ench1 < threeSubHalf)
                         if(slot1 != 1)
                             continue;
@@ -175,13 +303,41 @@ namespace Minecraft_Enchantment_Cracker.Tasks {
                     if(ench1 < slot1low || ench1 > slot1high)
                         continue;
 
-                    ench2 = (seed.NextTwoInt(shelvesPlusOne) + halfShelves) * 2 / 3;
+                    seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                    temprng = (int)((ulong)seed >> 45) & 7;
+
+                    seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                    temprng2 = (int)((ulong)seed >> 17);
+                    if((shelvesPlusOne & shelvesPlusOne - 1) == 0)
+                        temprng2 = (int)((shelvesPlusOne * (long)temprng2) >> 31);
+                    else
+                        while(temprng2 - (temprng2 %= shelvesPlusOne) + shelvesPlusOne - 1 < 0)
+                        {
+                            seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                            temprng2 = (int)((ulong)seed >> 17);
+                        }
+
+                    ench2 = (temprng + temprng2 + halfShelves) * 2 / 3;
                     if(ench2 != secondSubOne)
                         continue;
 
                     if(!early3)
                     {
-                        ench3 = (seed.NextTwoInt(shelvesPlusOne) + halfShelves);
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        temprng = (int)((ulong)seed >> 45) & 7;
+
+                        seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                        temprng2 = (int)((ulong)seed >> 17);
+                        if((shelvesPlusOne & shelvesPlusOne - 1) == 0)
+                            temprng2 = (int)((shelvesPlusOne * (long)temprng2) >> 31);
+                        else
+                            while(temprng2 - (temprng2 %= shelvesPlusOne) + shelvesPlusOne - 1 < 0)
+                            {
+                                seed = (seed * JavaRandom.M + 0xBL) & JavaRandom.Mask;
+                                temprng2 = (int)((ulong)seed >> 17);
+                            }
+
+                        ench3 = temprng + temprng2 + halfShelves;
                         if(Math.Max(ench3, twoShelves) != slot3)
                             continue;
                     }
@@ -189,9 +345,11 @@ namespace Minecraft_Enchantment_Cracker.Tasks {
                     result.Add(s);
                 }
             }
-            
+
             LastSeedsFound = result.Count;
-            if(LastSeedsFound == 1) TheSeed = result[0];
+            if(LastSeedsFound == 1)
+                TheSeed = result[0];
+
             LastSearchTime = Environment.TickCount - start;
             return result;
         }
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Minecraft_Enchantment_Cracker;
 using Minecraft_Enchantment_Cracker.Tasks;
 using NUnit.Framework;
 
@@ -9,28 +8,6 @@ namespace Tests
 {
     public static class Tests
     {
-        [TestFixture]
-        public static class RngTests
-        {
-            /// <summary>
-            /// This method tests the output of the NextInt method to ensure that it
-            /// successfully changes the seed and outputs the correct value
-            /// </summary>
-            /// <param name="seed">The input seed to the PRNG</param>
-            /// <param name="max">The maximum value passed to NextInt (exclusive)</param>
-            /// <param name="expectedSeed">The expected seed after one iteration of the PRNG</param>
-            /// <param name="expectedVal">The expected output of the NextInt call</param>
-            [TestCase(1337L, 420, 33712326537040L, 419)]
-            [TestCase(123456789L, 8, 119305093197820L, 3)]
-            [TestCase(999999999L, 1, 94003067820958L, 0)]
-            public static void NextInt_Tests(ref long seed, int max, long expectedSeed, int expectedVal)
-            {
-                var result = seed.NextInt(max);
-                Assert.AreEqual(expectedSeed, seed);
-                Assert.AreEqual(expectedVal, result);
-            }
-        }
-
         [TestFixture]
         public static class CrackingTests
         {
